7b051df2cb5f924a43416d2e36996df4aee73ada
rizunadiva <rizuananm@gmail.com>
2022-07-13 21:35:33 +0700

    1

---
+++ database/driver.go
@@ -0,0 +1,26 @@
+package mysql
+
+import (
+	"fmt"
+	"log"
+
+	"API2/model"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDB() *gorm.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True", "root", "Mihrimah220918", "localhost", 3306, "echorm")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Cannot connect to DB")
+	}
+
+	return db
+}
+
+func MigrateData(db *gorm.DB) {
+	db.AutoMigrate(model.User{}, model.Book{})
+}
